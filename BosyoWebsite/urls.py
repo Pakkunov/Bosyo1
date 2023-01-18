e4f23fe5e045b1fc169a23cdc8b98f2817dd15f6
Pakkunov <67741102+Pakkunov@users.noreply.github.com>
2023-01-18 15:47:33 +0800

    Update urls.py

--- BosyoWebsite/urls.py
+++ BosyoWebsite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name='BosyoWebsite'
+
 urlpatterns = [
     path('', include('website.urls'))
 ]
