da90e92b74635932098f73c6370815fb990a2fe5
Patto <rariexph@gmail.com>
2023-03-21 03:02:18 +0800

    update

--- BosyoWebsite/settings.py
+++ BosyoWebsite/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from pathlib import Path
 import environ
+from pathlib import Path
 
 # Initialise environment variables
 env = environ.Env()
