0194c7cc563d427bdba7fb86c4f0a8b276a2e129
Patto <rariexph@gmail.com>
2023-03-21 03:05:52 +0800

    update settings.py

--- BosyoWebsite/settings.py
+++ BosyoWebsite/settings.py
@@ -10,12 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import environ
 from pathlib import Path
-
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
