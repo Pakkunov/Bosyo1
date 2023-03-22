3ed2b4de6f11a811735488df1b1780b7d1e63862
Pakkunov <67741102+Pakkunov@users.noreply.github.com>
2023-03-22 11:24:44 +0800

    Update settings.py

--- BosyoWebsite/settings.py
+++ BosyoWebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mzd8m-qtnmvf^$4+8&gp#b7-r6x=at-m9h^ve08+#8^jh&#@c7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','bosyoappliku.applikuapp.com', 'bosyogarbagecollector.fly.dev', 'lionfish-app-nurjp.ondigitalocean.app', 'https://bosyogarbagecollector.com']
 
