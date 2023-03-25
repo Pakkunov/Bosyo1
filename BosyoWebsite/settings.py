b3b97e55e755915c15916cd259c9f0936f28b233
Pakkunov <67741102+Pakkunov@users.noreply.github.com>
2023-03-25 11:13:10 +0800

    not important

--- BosyoWebsite/settings.py
+++ BosyoWebsite/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bosyoappliku', 
         'USER': 'postgres', 
-        'PASSWORD': 'postadmin123',
+        'PASSWORD': '4everalone',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
